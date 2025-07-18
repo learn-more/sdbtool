9ad3626df86a888a826cef9c84f2af5b05701c04
Mark Jansen <mark.jansen@reactos.org>
2025-07-18 21:21:18 +0200

    Improve cli coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,9 @@ PURPOSE:     Tests for the cli module.
 COPYRIGHT:   Copyright 2025 Mark Jansen <mark.jansen@reactos.org>
 """
 
+import click
 from click.testing import CliRunner
+import sdbtool.cli
 from sdbtool.cli import sdbtool_command
 
 
@@ -15,3 +17,70 @@ def test_version():
         result = runner.invoke(sdbtool_command, ["--version"])
         assert result.exit_code == 0
         assert result.output.startswith("sdbtool, version ")
+
+
+def test_noargs():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(sdbtool_command)
+        assert result.exit_code == 2
+        assert "Usage: sdbtool" in result.output
+
+
+def test_sdb2xml_command(tmp_path, monkeypatch):
+    monkeypatch.setattr(
+        "sdbtool.sdb2xml.convert", lambda *args, **kwargs: click.echo("nop")
+    )
+    runner = CliRunner()
+    with runner.isolated_filesystem(tmp_path):
+        with open("test.sdb", "w") as f:
+            f.write("This is a test SDB file.")
+        result = runner.invoke(sdbtool_command, ["sdb2xml", "test.sdb"])
+        assert result.exit_code == 0
+        assert "nop\n" == result.output
+
+
+def test_sdb2xml_exception(tmp_path, monkeypatch):
+    def raise_value_error(*args, **kwargs):
+        raise ValueError("Test error")
+
+    monkeypatch.setattr("sdbtool.sdb2xml.convert", raise_value_error)
+    runner = CliRunner()
+    with runner.isolated_filesystem(tmp_path):
+        with open("test.sdb", "w") as f:
+            f.write("This is a test SDB file.")
+        result = runner.invoke(sdbtool_command, ["sdb2xml", "test.sdb"])
+        assert result.exit_code == 1
+        assert "Error converting SDB to XML" in result.output
+        assert "Test error" in result.output
+
+
+def test_attributes_command(tmp_path, monkeypatch):
+    def mock_get_attributes(file_name):
+        if file_name == "test2.sdb":
+            # Simulate returning a list of attributes
+            return ["Test Attribute1", "Test Attribute2"]
+        if file_name == "test3.sdb":
+            raise ValueError("Test Error")
+        return []
+
+    monkeypatch.setattr(sdbtool.cli, "get_attributes", mock_get_attributes)
+    runner = CliRunner()
+    with runner.isolated_filesystem(tmp_path):
+        with open("test1.sdb", "w") as f:
+            f.write("This is a test SDB file.")
+        with open("test2.sdb", "w") as f:
+            f.write("This is a test SDB file.")
+        with open("test3.sdb", "w") as f:
+            f.write("This is a test SDB file.")
+        result = runner.invoke(
+            sdbtool_command,
+            ["attributes", "test1.sdb", "test2.sdb", "test3.sdb"],
+        )
+        assert result.exit_code == 0
+        assert "Attributes for test1.sdb:" in result.output
+        assert "No attributes found." in result.output
+        assert "Attributes for test2.sdb:" in result.output
+        assert "  Test Attribute1" in result.output
+        assert "  Test Attribute2" in result.output
+        assert "Error getting attributes for test3.sdb: Test Error" in result.output
